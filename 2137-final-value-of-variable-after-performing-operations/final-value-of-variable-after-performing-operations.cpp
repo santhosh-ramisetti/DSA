41eb49183fb6812145536929c284e599509c2fb0
santhosh-ramisetti <stdio.santhosh@gmail.com>
2025-04-03 20:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (74.82%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& o) {
+        int x=0;
+        for(int i=0;i<o.size();i++)
+        {
+            if(o[i]=="X++" or o[i]=="++X")
+            {
+                x++;
+            }
+            else
+            {
+                x--;
+            }
+        }
+        return x;
+    }
+};
